ea7b4c1b087821ac2d909e882b144739edf9296d
grayzone <Albert.wang@covidien.com>
2016-04-13 13:45:31 +0800

    remove config package.

--- config/osconfig.go
+++
@@ -1,8 +0,0 @@
-package config
-
-const (
-	/* Define to the version number of this package. */
-	PACKAGE_VERSION_NUMBER = "360"
-	/* Define to the version of this package. */
-	PACKAGE_VERSION = "3.6.0"
-)
