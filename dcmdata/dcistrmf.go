2a6a51f96e6a1872a238c07a206a42ffe2315e88
grayzone <Albert.wang@covidien.com>
2016-04-13 13:33:28 +0800

    add function and test cases in dcistrmf.

--- dcmdata/dcistrmf.go
+++ dcmdata/dcistrmf.go
@@ -20,18 +20,42 @@ type DcmInputFileStream struct {
 func NewDcmFileProducer(filename string, offset int64) *DcmFileProducer {
 	var result DcmFileProducer
 	result.status_ = ofstd.EC_Normal
-	var err error
-	result.file_, err = os.Open(filename)
-	defer result.file_.Close()
+	err := result.Open(filename)
 	if err != nil {
 		result.status_ = ofstd.MakeOFCondition(OFM_dcmdata, 18, ofstd.OF_error, err.Error())
 		return &result
 	}
-	result.size_, _ = result.file_.Seek(offset, os.SEEK_END)
+	result.size_, _ = result.file_.Seek(0, os.SEEK_END)
 	_, err = result.file_.Seek(offset, os.SEEK_SET)
 	if err != nil {
 		result.status_ = ofstd.MakeOFCondition(OFM_dcmdata, 18, ofstd.OF_error, err.Error())
 		return &result
 	}
 	return &result
+}
+
+func (p *DcmFileProducer) Open(filename string) error {
+	var err error
+	p.file_, err = os.Open(filename)
+	return err
+}
+
+func (p *DcmFileProducer) Close() error {
+	return p.file_.Close()
+}
+
+func (p *DcmFileProducer) Good() bool {
+	return p.status_.Good()
+}
+
+func (p *DcmFileProducer) Status() ofstd.OFCondition {
+	return p.status_
+}
+
+func (p *DcmFileProducer) Eos() bool {
+	if p.file_ == nil {
+		return true
+	}
+	size, _ := p.file_.Seek(0, os.SEEK_CUR)
+	return size == p.size_
 }
--- dcmdata/dcistrmf_test.go
+++ dcmdata/dcistrmf_test.go
@@ -28,9 +28,77 @@ func TestNewDcmFileProducer(t *testing.T) {
 	}
 	for _, c := range cases {
 		got := NewDcmFileProducer(c.in_1, c.in_2)
+		defer got.Close()
+		defer c.want.Close()
 
 		if (got.size_ != c.want.size_) || (got.status_.Status() != c.want.status_.Status()) {
 			t.Errorf("NewDcmFileProducer(%v,%v) == want %v got %v", c.in_1, c.in_2, c.want, got)
 		}
 	}
+}
+
+func TestDcmFileProducerGood(t *testing.T) {
+	cases := []struct {
+		in   *DcmFileProducer
+		want bool
+	}{
+		{NewDcmFileProducer("", 0), false},
+		{NewDcmFileProducer(gettestdatafolder()+"GH220.dcm", 0), true},
+		{NewDcmFileProducer(gettestdatafolder()+"GH223.dcm", 0), true},
+		{NewDcmFileProducer(gettestdatafolder()+"GH133.dcm", 0), true},
+	}
+	for _, c := range cases {
+		got := c.in.Good()
+		defer c.in.Close()
+
+		if got != c.want {
+			t.Errorf(" %v Good() == want %v got %v", c.in, c.want, got)
+		}
+	}
+
+}
+
+func TestDcmFileProducerStatus(t *testing.T) {
+	cases := []struct {
+		in   *DcmFileProducer
+		want ofstd.OFCondition
+	}{
+		{NewDcmFileProducer("", 0), ofstd.MakeOFCondition(OFM_dcmdata, 18, ofstd.OF_error, "open : The system cannot find the file specified.")},
+		{NewDcmFileProducer(gettestdatafolder()+"GH220.dcm", 0), ofstd.EC_Normal},
+		{NewDcmFileProducer(gettestdatafolder()+"GH223.dcm", 0), ofstd.EC_Normal},
+		{NewDcmFileProducer(gettestdatafolder()+"GH133.dcm", 0), ofstd.EC_Normal},
+	}
+	for _, c := range cases {
+		got := c.in.Status()
+		defer c.in.Close()
+
+		if got.Status() != c.want.Status() {
+			t.Errorf(" %v Status() == want %v got %v", c.in, c.want, got)
+		}
+	}
+
+}
+
+func TestDcmFileProducerEos(t *testing.T) {
+	cases := []struct {
+		in   *DcmFileProducer
+		want bool
+	}{
+		{NewDcmFileProducer("", 0), true},
+		{NewDcmFileProducer(gettestdatafolder()+"GH220.dcm", 0), false},
+		{NewDcmFileProducer(gettestdatafolder()+"GH223.dcm", 0), false},
+		{NewDcmFileProducer(gettestdatafolder()+"GH133.dcm", 0), false},
+		{NewDcmFileProducer(gettestdatafolder()+"GH220.dcm", 454), true},
+		{NewDcmFileProducer(gettestdatafolder()+"GH223.dcm", 702), true},
+		{NewDcmFileProducer(gettestdatafolder()+"GH133.dcm", 2980438), true},
+	}
+	for _, c := range cases {
+		got := c.in.Eos()
+		defer c.in.Close()
+
+		if got != c.want {
+			t.Errorf(" %v Eos() == want %v got %v", c.in, c.want, got)
+		}
+	}
+
 }
