a9290daba4f2bbae82dcb2c2ed09c750e1aede71
grayzone <Albert.wang@covidien.com>
2016-04-13 11:24:22 +0800

    fix errors in test case.

--- dcmdata/dcistrmf_test.go
+++ dcmdata/dcistrmf_test.go
@@ -22,14 +22,14 @@ func TestNewDcmFileProducer(t *testing.T) {
 		want *DcmFileProducer
 	}{
 		{"", 0, &DcmFileProducer{status_: ofstd.MakeOFCondition(OFM_dcmdata, 18, ofstd.OF_error, "open : The system cannot find the file specified."), size_: 0}},
-		{gettestdatafolder() + "gh220.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 454}},
-		{gettestdatafolder() + "gh223.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 702}},
-		{gettestdatafolder() + "gh133.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 2980438}},
+		{gettestdatafolder() + "GH220.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 454}},
+		{gettestdatafolder() + "GH223.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 702}},
+		{gettestdatafolder() + "GH133.dcm", 0, &DcmFileProducer{status_: ofstd.EC_Normal, size_: 2980438}},
 	}
 	for _, c := range cases {
 		got := NewDcmFileProducer(c.in_1, c.in_2)
 
-		if (got.size_ != c.want.size_) || (got.status_ != c.want.status_) {
+		if (got.size_ != c.want.size_) || (got.status_.Status() != c.want.status_.Status()) {
 			t.Errorf("NewDcmFileProducer(%v,%v) == want %v got %v", c.in_1, c.in_2, c.want, got)
 		}
 	}
